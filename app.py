35045352322028d28a3ca885d1f5ba7eb8d266a9
Uthmanade001 <Adeleyetemilade001@gmail.com>
2025-06-18 01:18:00 +0100

    Add default values for DB connection

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
+#application to connect to PostgreSQL using Flask and psycopg2
 from flask import Flask
 import psycopg2
 import os
 
 app = Flask(__name__)
-
+# Function to get a database connection
 def get_db_connection():
     conn = psycopg2.connect(
-        host=os.getenv("POSTGRES_HOST"),
-        database=os.getenv("POSTGRES_DB"),
-        user=os.getenv("POSTGRES_USER"),
-        password=os.getenv("POSTGRES_PASSWORD")
+        host=os.getenv("POSTGRES_HOST", "db"),
+        database=os.getenv("POSTGRES_DB", "mydb"),
+        user=os.getenv("POSTGRES_USER", "myuser"),
+        password=os.getenv("POSTGRES_PASSWORD", "mypassword")
     )
     return conn
 
